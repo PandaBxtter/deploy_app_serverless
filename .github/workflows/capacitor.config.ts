19ace41bd2d97c08ab41d7d2337c96058b0fce68
12AMJ0SH <137812095+PandaBxtter@users.noreply.github.com>
2025-05-03 19:40:29 -0400

    Create capacitor.config.ts

---
+++ .github/workflows/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.editorapp',
+  appName: 'AscentCode',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
